7fe55840ae38697ee9323d44d5d78443e72e4d4e
Anirban Kapil <anirbankapil@gmail.com>
2025-08-14 17:06:23 +0530

    add cors middleware , update package dependencies and standard ApiResponse and ApiError

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./app.js";
 
 dotenv.config({
     path : "./.env"
 });
 
 
-const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-
-app.get("/",(req,res)=>{
-    res.send("Welcome")
-})
 
 app.listen(port,()=>{
-    console.log(`App Up and Running on port - ${port}`)
+    console.log(`App up and running on port -- ${port}`)
 })
+
---
+++ src/utils/api-error.js
@@ -0,0 +1,24 @@
+
+class ApiError {
+    constructor(
+        statuscode,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statuscode = statuscode;
+        this.message = message;
+        this.errors = errors;
+        this.success = false;
+        this.data = null;
+        
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
