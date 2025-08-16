22105bd6e3a171f2771abdaf0a4186a6e2dd0773
Anirban Kapil <anirbankapil@gmail.com>
2025-08-16 20:57:04 +0530

    implement MongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path : "./.env"
@@ -9,7 +10,15 @@ dotenv.config({
 const port = process.env.PORT || 3000
 
 
-app.listen(port,()=>{
+connectDB()
+.then(()=>{
+    app.listen(port,()=>{
     console.log(`App up and running on port -- ${port}`)
 })
+})
+.catch((err)=>{
+    console.log("Error connecting to MnogoDB")
+})
+
+
 
