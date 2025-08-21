063d4a638268fcffc4dec956382afa7107fa3f9a
Anirban Kapil <anirbankapil@gmail.com>
2025-08-21 20:38:16 +0530

    send email  service

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,5 +1,48 @@
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
 
+const sendEmail = async (options) => {
+    const mailGenerator = new Mailgen(
+        {
+            theme : "default",
+            product : {
+                name : "Task Manager",
+                link : "https://taskmanagerlink.com"
+            }
+        }
+    )
+
+    const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent)
+
+    const emailHtml = mailGenerator.generate(options.mailgenContent)
+
+
+    const transporter = nodemailer.createTransport(
+        {
+            host : process.env.MAILTRAP_SMTP_HOST,
+            port : process.env.MAILTRAP_SMTP_PORT,
+            auth : {
+                user : process.env.MAILTRAP_SMTP_USER,
+                pass : process.env.MAILTRAP_SMTP_PASS
+            } 
+        }
+    );
+
+    const mail = {
+        from : "mail.taskmanager@example.com",
+        to : options.email,
+        subject : options.subject,
+        text : emailTextual,
+        html : emailHtml
+    };
+
+    try {
+        await transporter.sendMail(mail);
+    } catch (error) {
+        console.error("Email service failed siliently.Make sure to provide mailtrap credentials");
+        console.error("error-",error);
+    }
+};
 
 
 const emailVerificationMailgenContent = (username , verificationUrl) => {
@@ -38,3 +81,5 @@ const forgotPasswordMailgenContent = (username , passwordVerificationUrl) => {
         }
     }
 }
+
+export {emailVerificationMailgenContent , forgotPasswordMailgenContent , sendEmail}
