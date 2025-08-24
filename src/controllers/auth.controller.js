ec24a34c0de623291b16844e4dbca819d59ba14b
Anirban Kapil <anirbankapil@gmail.com>
2025-08-24 21:07:16 +0530

    update email verification process in registerUser function

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {sendEmail} from "../utils/mail.js"
+import {emailVerificationMailgenContent, sendEmail} from "../utils/mail.js"
 
 const generateAccessAndRefreshToken = async (userId) => {
     
@@ -52,6 +52,25 @@ const registerUser = asyncHandler(async (req,res) => {
 
     await sendEmail({
         email : user?.email,
-        
+        subject : "Please verify your email",
+        maigenContent : emailVerificationMailgenContent(
+            user.username,
+            `${req.protocol}://${req.host}/api/v1/users/verify-email/${unHashedToken}`
+        )
     })
-})
+
+    const createdUser = await User.findById(user._id).select(
+        " -password -refreshToken -emailVerificationToken -emailVerificationExpiry"
+    );
+    if(!createdUser){
+        throw new ApiError(500,"Error while registering a user")
+    };
+    
+    return res.status(201).json(
+        new ApiResponse(201,{user : createdUser},"User registered successfully. Please verify your email")
+    );
+
+});
+
+
+export {registerUser};
