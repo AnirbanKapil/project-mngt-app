39dd0e11d478e2fdb4c2dab6db845fa234b2485b
Anirban Kapil <anirbankapil@gmail.com>
2025-09-30 12:12:36 +0530

    fix: correct typo

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ import { ApiError } from "../utils/api-error.js";
 import {jwt} from "jsonwebtoken"; 
 
 export const verifyJWT = asyncHandler( async (req,res,next) => {
-      const token = req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer ","");
+      const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
 
       if(!token){
         throw new ApiError(401,"Unauthorized request")
